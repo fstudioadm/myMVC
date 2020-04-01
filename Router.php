4effd7d08a8363be2361c0e7dac9106d9055631c
Zilola <62849764+zry77@users.noreply.github.com>
2020-04-01 21:25:25 +0500

    Router and functions

---
+++ Router.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Router{
+
+    // public function __construct(){
+    //     echo 'Привет мир!';
+    // }
+
+
+    protected static $routes = []; 
+    protected static $route = [];
+
+    public static function add($regexp, $route = []){
+        self::$routes[$regexp] = $route;
+    }
+
+    public static function getRoutes(){
+        return self::$routes;
+    }
+
+    public static function getRoute(){
+        return self::$route;
+    }
+
+    public static function matchRoute($url){
+        foreach(self::$routes as $pattern => $route){
+            if()
+
+        }
+    }
+
+}
+
+
